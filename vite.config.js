5dfbc019b72d1d9894948c3c3514e27b55231ab1
Jesse Ekene <jesse21hundred@gmail.com>
2025-08-25 14:28:58 +0100

    updated files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chuffverse',   // 👈 Important for GitHub Pages + custom domain
+  base: '/chuffverse/',   // 👈 Important for GitHub Pages + custom domain
 })
